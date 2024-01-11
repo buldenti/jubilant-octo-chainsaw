247a7606226c90cba0fe4621ea3da91ac37a264f
buldenti <59711896+buldenti@users.noreply.github.com>
2024-01-11 13:05:33 -0500

    added p5.js example from website

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
- // put setup code here
+  createCanvas(720, 400);
+  noStroke();
+  rectMode(CENTER);
 }
 
 function draw() {
-  // put drawing code here
+  background(230);
+  fill(244, 122, 158);
+  rect(mouseX, height / 2, mouseY / 2 + 10, mouseY / 2 + 10);
+  fill(237, 34, 93);
+  let inverseX = width - mouseX;
+  let inverseY = height - mouseY;
+  rect(inverseX, height / 2, inverseY / 2 + 10, inverseY / 2 + 10);
 }
